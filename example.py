aabc83bca1c4b64811f5ee59ee71ae676326a130
= <=>
2014-05-11 11:01:32 +0200

    Updated example

--- example.py
+++ example.py
@@ -10,4 +10,7 @@ class ExampleTable(Table):
 
 table = ExampleTable.open("example.csv")
 print table.group_by("department")
+print table.group_by("department", key="name")
+print table.group_by("department", function=sum, key="salary")
+table.filter(department="IT").update(salary=2700)
 print table.group_by("department", function=sum, key="salary")
